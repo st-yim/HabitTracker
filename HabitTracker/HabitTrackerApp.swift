e7c9a9a442b0549938c4ee8993c2e019b06b14b8
Purvesh Dodiya <psdodiyaldce@gmail.com>
2024-01-06 17:44:10 +0530

    Initial setup

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Steven Yim on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitTracker/Model/HabitModel.swift
@@ -0,0 +1,29 @@
+//
+//  HabitModel.swift
+//  HabitTracker
+//
+//  Created by Steven Yim on 1/4/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct Habit: Identifiable {
+    let id = UUID()
+    let imageName: String
+    var title: String
+    var isSelected: Bool
+    var isActive = true
+    var description: String?
+}
+
+struct CreatedHabit: Identifiable {
+    let id = UUID()
+    let imageName: String
+    var title: String
+    var isSelected: Bool
+    var isActive = true
+    var description: String?
+}
+
+
